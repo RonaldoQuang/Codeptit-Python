507c172caf104f0b2cdea8ee4ac236917aff2ead
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-10-10 17:27:18 +0700

    Update PY01015-SO KHONG GIAM.py

--- PY01015-SO KHONG GIAM.py
+++ PY01015-SO KHONG GIAM.py
@@ -1,3 +1,20 @@
+"""
+Một số được gọi là số không giảm nếu không có cặp chữ số cạnh nhau (i, i+1) nào mà số thứ i lớn hơn số thứ i+1.
+Viết chương trình kiểm tra số nguyên dương có thỏa mãn là số không giảm hay không.
+Input
+Dòng đầu ghi số bộ test (không quá 10).
+Mỗi dòng ghi một số nguyên dương (không quá 500 chữ số).
+Output
+Ghi ra YES nếu đó là số không giảm. NO nếu ngược lại
+Input
+2
+12345678888888888888889
+65645645465754765876857685846
+Output
+YES
+NO
+"""
+
 from math import *
 
 def check(s):
