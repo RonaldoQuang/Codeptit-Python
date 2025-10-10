3f9565cf8e1f8364a4140400df339e79a49fdda2
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-10-10 09:57:42 +0700

    Update PY02066-BAI TOAN DEM.py

--- PY02066-BAI TOAN DEM.py
+++ PY02066-BAI TOAN DEM.py
@@ -2,7 +2,9 @@
 Ngày thi chung kết ICPC PTIT 2020, các lập trình viên đang say sưa đọc đề và fix bug trong Hội trường 1, nhưng từ Ký túc xá thì nhóm bạn nữ xinh đẹp nào đó liên tục đồng thanh lặp đi lại lại câu hát quen thuộc:
 “1, 2, 3, 5 anh có đánh rơi nhịp nào không?
 Nếu câu trả lời là có …”
-Để cho phù hợp với tình hình thời sự và giảm bớt căng thẳng cho các bạn thí sinh, ban tổ chức quyết định thêm một bài toán đơn giản: cho một dãy các số nguyên đếm tăng dần. Hỏi có số nào bị “đánh rơi” khi đếm hay không? Giả sử một dãy đếm chính xác thì luôn luôn đếm bắt đầu từ 1.
+Để cho phù hợp với tình hình thời sự và giảm bớt căng thẳng cho các bạn thí sinh, ban tổ chức quyết định thêm một bài toán đơn giản: cho một dãy các số nguyên đếm tăng dần. 
+Hỏi có số nào bị “đánh rơi” khi đếm hay không? 
+Giả sử một dãy đếm chính xác thì luôn luôn đếm bắt đầu từ 1.
 Dữ liệu vào:
 Dòng đầu ghi số N là số con số được đếm (1 ≤ N ≤ 100)
 Các dòng tiếp theo ghi đủ N số A[i] theo thứ tự tăng dần (1 ≤ A[i] ≤ 200). Các số phân cách bởi khoảng trống hoặc xuống dòng.
