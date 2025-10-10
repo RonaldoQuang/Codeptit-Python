e33c7661cb0069ed2a341f6b8e5000619ef27f82
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-10-10 17:19:45 +0700

    Update PY01002-PHEP CONG.py

--- PY01002-PHEP CONG.py
+++ PY01002-PHEP CONG.py
@@ -1,3 +1,15 @@
+"""
+Cho một phép toán có dạng a + b = c với a,b,c chỉ là các số nguyên dương có một chữ số.
+Hãy kiểm tra xem phép toán đó có đúng hay không.
+Input
+Chỉ có một dòng ghi ra phép toán (gồm đúng 9 ký tự)
+Ouput
+Ghi ra YES nếu phép toán đó đúng. Ghi ra NO nếu sai.
+Input
+1 + 2 = 3
+Output
+YES
+"""
 
 if __name__ == '__main__':
     s=input()
