6d879c1f43fbe4ef20fd547919bd25cf059d39b4
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-10-10 17:24:59 +0700

    Update PY01013-UOC SO CHUNG NGUYEN TO.py

--- PY01013-UOC SO CHUNG NGUYEN TO.py
+++ PY01013-UOC SO CHUNG NGUYEN TO.py
@@ -1,3 +1,23 @@
+"""
+Cho hai số nguyên dương a và b. 
+Hãy kiểm tra xem ước số chung lớn nhất của hai số này có tổng chữ số là nguyên tố hay không.
+Ví dụ a = 42, b = 28, ước số chung lớn nhất = 14. 
+Tổng chữ số của ước số chung là 1+4=5 là một số nguyên tố.
+Input
+Dòng đầu ghi số bộ test. Mỗi test ghi trên một dòng hai số nguyên dương a,b (không quá 6 chữ số)
+Output
+Ghi ra YES hoặc NO tùy thuộc kết quả kiểm tra
+Input
+3
+28 42
+123 18
+550 55
+Output
+YES
+YES
+NO
+"""
+
 from math import *
 
 def nto(n):
